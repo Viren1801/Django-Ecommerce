24eb7407fb98166a99aee1b7ed4387fa43d1e462
= <=>
2021-04-22 12:53:38 +0530

    The user tabs are added only

---
+++ custom_admin/models.py
@@ -0,0 +1,27 @@
+from datetime import date, datetime
+
+from django.conf import settings
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+	email = models.EmailField(_('email address'), unique=True,null=False,blank=False)
+	first_name = models.CharField(_('first name'), max_length=30, blank=True)
+	last_name = models.CharField(_('last name'), max_length=30, blank=True)
+	date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+	is_active = models.BooleanField(_('active'), default=True)
+	is_staff = models.BooleanField(_('staff status'),default=False)
+
+
+	objects = UserManager()
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
---
+++ custom_admin/urls.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.urls import path, re_path
+from . import views
+
+app_name = 'custom_admin'
+urlpatterns = [
+path('', views.index, name='index'),
+path('login/',views.login, name = 'login'),
+#path('register/',views.register, name = 'register'),
+path('logout/',views.logoutUser, name = 'logout'),
+
+# user management
+path('users/', views.UserList, name='UserList'),
+path('users/user_add', views.UserAdd, name='UserAdd'),
+path('users/user_update/<int:id>', views.UserUpdate, name='UserUpdate'),
+path('users/user_delete/<int:id>', views.UserDelete, name='UserDelete'),
+path('users/user_change', views.UserChange, name='UserChange'),
+path('users/user_change_psw', views.UserChangePsw, name='UserChangePsw'),
+]
